fd0b4b048b9bb9066f31fc249992f4a4425fc8bd
yeonssu <dustn7197@naver.com>
2023-01-20 17:26:16 +0900

    algorithmPractice

--- src/Greedy/Money2.java
+++ src/Greedy/Money2.java
@@ -1,6 +1,6 @@
 package Greedy;
 
-public class Money {
+public class Money2 {
     public static void main(String[] args) {
         // 4000원을 받았을 때 500원짜리 동전 8개를 반환합니다.
         int output1 = partTimeJob(4000);
@@ -11,31 +11,7 @@ public class Money {
         System.out.println(output2); // --> 18
     }
     public static int partTimeJob(int k) {
-//        int result = 0;
-//
-//        while (k>0){
-//            if (k % 500 == 0){
-//                result+=k/500;
-//                k=0;
-//            } else if (k % 100 == 0){
-//                k-=100;
-//                result++;
-//            } else if (k % 50 == 0) {
-//                k-=50;
-//                result++;
-//            } else if (k % 10 == 0) {
-//                k-=10;
-//                result++;
-//            } else if (k % 5 == 0) {
-//                k-=5;
-//                result++;
-//            }else {
-//                k -= 1;
-//                result ++;
-//            }
-//        }
-//
-//        return result;
+        //sol2)
         int[] coins = new int[]{1,5,10,50,100,500};
         int last = coins.length - 1;
         int count = 0;
